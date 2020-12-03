713b28601eb3a211ef3b0da9b8d3c187ea570bc2
xmetropol <xmetropol@zaaml.com>
2020-12-03 17:37:29 +0300

    Getting started tutorial

---
+++ source/Tutorials/GettingStarted/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Zaaml.PresentationCore.Theming;
+using Zaaml.Theming;
+
+namespace GettingStarted
+{
+  public partial class App
+  {
+    public App()
+    {
+      // Enable Metro Office theme
+      ThemeManager.ApplicationTheme = Themes.MetroOffice;
+    }
+  }
+}
---
+++ source/Tutorials/GettingStarted/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Zaaml.UI.Controls.ListView;
+
+namespace GettingStarted
+{
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    public List<string> ListData { get; } = new List<string>(Enumerable.Range(0, 1000).Select(i => $"List Item {i}"));
+  }
+
+  public sealed class ListViewFilter : ListViewItemTextFilter<string>
+  {
+    protected override bool Pass(string item)
+    {
+      return item.Contains(FilterText);
+    }
+  }
+}
